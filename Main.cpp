7eeb08597bd2085642600cae2b5a2c00c5fc210b
Максим Данченко <127301792+GrimFoxRF@users.noreply.github.com>
2023-08-22 11:32:30 +0300

    Добавил основные файлы программы

---
+++ Main.cpp
@@ -0,0 +1,49 @@
+﻿//алгоритм сортировки массива в одном потоке и в нескольких потоках
+#include <Windows.h>
+#include <ctime>
+#include "MergeSort.h"
+
+int main() 
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+
+	int numCores = coreNumber();//количество ядер CPU
+	std::cout << "\nКоличество ядер CPU: " << numCores << std::endl;
+	int maxThreads = numCores;//устанавливает максимальное число потоков
+	int activeThreads = 0;//счетчик активных потоков
+
+	std::time_t start1 = 0;
+	std::time_t	end1 = 0;
+	double seconds1 = 0;
+
+	std::vector<int> largeVector1; //создаем первый большой вектор
+	std::vector<int> largeVector2; //создаем второй большой вектор
+
+	const long long size = 50'000'000;//размер вектора
+
+	largeVectorFill(largeVector1, size); //заполняем вектор 1 случайными числами от 0 до 100
+	largeVectorFill(largeVector2, size); //заполняем вектор 2 случайными числами от 0 до 100
+
+	std::time(&start1);
+	mergeSortInOneThread(largeVector1, 0, largeVector1.size() - 1);//сортировка в одном потоке
+	std::time(&end1);
+	seconds1 = std::difftime(end1, start1);
+	std::cout << "\nВремя выполнения сортировки в одном потоке: " << seconds1 << " секунд" << std::endl;
+
+	std::time_t start2 = 0;
+	std::time_t end2 = 0;
+	double seconds2 = 0;
+
+	std::time(&start2);
+	mergeSortInManyThreads(largeVector2, 0, largeVector2.size() - 1, maxThreads, activeThreads);//многопоточная сортировка
+	std::time(&end2);
+	seconds2 = std::difftime(end2, start2);
+	std::cout << "Время выполнения сортировки в " << maxThreads << " потоках: " << seconds2 << " секунд" << std::endl;
+	//printArray(largeVector2); //тест для вывода всего массива на экран, не использовать с огромными массивами
+
+	calculateRatio(seconds1, seconds2);//во сколько раз один метод быстрее другого
+
+	return 0;
+}
---
+++ MergeSort.cpp
@@ -0,0 +1,142 @@
+﻿#include "MergeSort.h"
+#include <cstdlib>
+#include <future>
+#include <mutex>
+
+std::mutex activeThreadsMutex;//мьютекс для переменной активных потоков
+//заполнение вектора элементами
+void largeVectorFill(std::vector<int>& largeVector, long long size)
+{
+	largeVector.reserve(size);//резервируем память под вектор
+
+	std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+	std::cout << "\nЗаполняем массив случайными числами" << std::endl;
+
+	for (int i = 0; i < size; i++) 
+	{
+		largeVector.push_back(std::rand()%101);
+	}
+
+	std::cout << "Массив размером [" << size << "] заполнен случайными числами" << std::endl;
+}
+//слияние подмассивов в один
+void merge(std::vector<int>& array, int left, int middle, int right)
+{
+    int sizeLeft = middle - left + 1;
+    int sizeRight = right - middle;
+
+    std::vector<int> leftArray(sizeLeft);
+    std::vector<int> rightArray(sizeRight);
+
+    for (int i = 0; i < sizeLeft; ++i)
+        leftArray[i] = array[left + i];
+    for (int j = 0; j < sizeRight; ++j)
+        rightArray[j] = array[middle + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < sizeLeft && j < sizeRight) {
+        if (leftArray[i] <= rightArray[j]) {
+            array[k] = leftArray[i];
+            ++i;
+        }
+        else {
+            array[k] = rightArray[j];
+            ++j;
+        }
+        ++k;
+    }
+
+    while (i < sizeLeft) {
+        array[k] = leftArray[i];
+        ++i;
+        ++k;
+    }
+
+    while (j < sizeRight) {
+        array[k] = rightArray[j];
+        ++j;
+        ++k;
+    }
+}
+//сортировка в одном потоке
+void mergeSortInOneThread(std::vector<int>& array, int left, int right)
+{
+    if (left < right) {
+        int middle = left + (right - left) / 2;
+
+        mergeSortInOneThread(array, left, middle);
+        mergeSortInOneThread(array, middle + 1, right);
+
+        merge(array, left, middle, right);
+    }
+}
+//многопоточная сортировка
+void mergeSortInManyThreads(std::vector<int>& array, int left, int right, int& maxThreads, int& activeThreads)
+{
+    if (left < right) {
+        int middle = left + (right - left) / 2;
+
+        std::unique_lock<std::mutex> mutexLock(activeThreadsMutex);
+        if (activeThreads < maxThreads) {
+            ++activeThreads;
+            mutexLock.unlock();
+
+            std::future<void> leftFuture = std::async(std::launch::async, [&] {
+                mergeSortInManyThreads(array, left, middle, maxThreads, activeThreads);
+                });
+            std::future<void> rightFuture = std::async(std::launch::async, [&] {
+                mergeSortInManyThreads(array, middle + 1, right, maxThreads, activeThreads);
+                });
+
+            leftFuture.get();
+            rightFuture.get();
+
+            mutexLock.lock();
+            --activeThreads;
+        }
+        else {
+            mutexLock.unlock();
+
+            mergeSortInManyThreads(array, left, middle, maxThreads, activeThreads);
+            mergeSortInManyThreads(array, middle + 1, right, maxThreads, activeThreads);
+        }
+
+        merge(array, left, middle, right);
+    }
+}
+//получение числа ядер системы
+int coreNumber()
+{
+    std::cout << "\nВычисляем количество ядер CPU..." << std::endl;
+    int numCores = std::thread::hardware_concurrency();
+    if (numCores > 0) {
+        
+        return numCores;
+    }
+    else {
+        std::cout << "\nОШИБКА: вычисления количества ядер CPU\n" << std::endl;
+        return 0;
+    }
+}
+//разница в скоросте между разными методами
+void calculateRatio(double t1, double t2)
+{
+    if (t1 < t2)
+    {
+        std::cout << "\nОднопоточная сортировка в " << t2 / t1 << " раз быстрее многопоточной" << std::endl;
+    }
+    else 
+    {
+        std::cout << "\nМногопоточная сортировка в " << t1 / t2 << " раз быстрее однопоточной" << std::endl;
+    }
+}
+//вывод содержимого массива в консоль (лучше не использовать с огромными массивами)
+void printArray(const std::vector<int>& array)
+{
+    for (const int& num : array) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
